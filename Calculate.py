a3fd60363c57391cdcb0296893ea8b3d620050ff
ApaBird <apa.birds@mail.ru>
2023-12-05 22:01:44 +0800

    develop crah

--- Calculate.py
+++ Calculate.py
@@ -1,36 +1,6 @@
-def addition(a, b):
-    try:
-        c = a + b
-        return c
-    except Exception:
-        print("Что то не складывается")
-        return None
+# Я устал, я ухожу
 
-
-def subtraction(a, b):
-    try:
-        c = a - b
-        return c
-    except Exception:
-        print("Что то не вычитается")
-        return None
-
-
-def multiplication(a, b):
-    try:
-        c = a * b
-        return c
-    except Exception:
-        print("Что то не умножается")
-        return None
-
-
-def division(a, b):
-    try:
-        c = a / b
-        return c
-    except Exception:
-        print("Что то не делится")
-        return None
+# TODO
+# Осталось доделать // ** % |
 
 
